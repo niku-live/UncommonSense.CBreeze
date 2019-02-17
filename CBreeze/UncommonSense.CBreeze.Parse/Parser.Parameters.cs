3cc83ca58922faeefb3f78cb0e2d568409fdc3d5
Karolis Uosis <nku2007@gmail.com>
2019-02-17 18:38:31 +0200

    Fix for parameters parsing bug when option type has ";" character in value, e.g. Test@1 : 'A,B,A;B'

--- CBreeze/UncommonSense.CBreeze.Parse/Parser.Parameters.cs
+++ CBreeze/UncommonSense.CBreeze.Parse/Parser.Parameters.cs
@@ -7,6 +7,55 @@ namespace UncommonSense.CBreeze.Parse
 {
     public partial class Parser
     {
+        private IEnumerable<string> IntelligentParamsSplit(string actualValue)
+        {
+            int position = actualValue.IndexOf(';');
+            if (position < 0)
+            {
+                yield return actualValue;
+                yield break;
+            }
+            int qPosition = actualValue.IndexOf('\'');
+            if (qPosition < 0)
+            {
+                foreach (var item in actualValue.Split(";".ToCharArray()))
+                {
+                    yield return item;
+                }
+                yield break;
+            }
+            StringBuilder aggr = new StringBuilder();
+            bool textMode = false;
+            foreach (var symbol in actualValue)
+            {
+                switch (symbol)
+                {
+                    case '\'':
+                        textMode = !textMode;
+                        aggr.Append(symbol);
+                        break;
+                    case ';':
+                        if (textMode)
+                        {
+                            aggr.Append(symbol);
+                        }
+                        else
+                        {
+                            yield return aggr.ToString();
+                            aggr.Clear();
+                        }
+                        break;
+                    default:
+                        aggr.Append(symbol);
+                        break;
+                }
+            }
+            if (aggr.Length > 0)
+            {
+                yield return aggr.ToString();
+            }
+        }
+
         internal void ParseParameters(Lines lines)
         {
             var match = lines.FirstLineMustMatch(Patterns.ProcedureParameters);
@@ -15,7 +64,7 @@ namespace UncommonSense.CBreeze.Parse
             if (string.IsNullOrEmpty(parameters))
                 return;
 
-            foreach (var parameter in parameters.Split(";".ToCharArray()))
+            foreach (var parameter in IntelligentParamsSplit(parameters))
             {
                 ParseParameter(parameter);
             }
