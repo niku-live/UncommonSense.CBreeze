bf4dc54959daeb76a14116c353119ca758003c4a
Karolis Uosis <nku2007@hotmail.com>
2019-03-09 11:08:55 +0200

    Possibility to choose different folder from command line

--- CBreeze/UncommonSense.CBreeze.Parse.Demo/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Parse.Demo/Program.cs
@@ -12,7 +12,8 @@ namespace UncommonSense.CBreeze.Parse.Demo
         {
             var myListener = new MyListener();
             var desktopFolderName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            var sourceFolderName = Path.Combine(desktopFolderName, "be");
+            string folderFromArgs = (args != null) && (args.Length > 0) ? args[0] : null;
+            var sourceFolderName = folderFromArgs ?? Path.Combine(desktopFolderName, "be");
 
             var parser = new Parser();
             parser.Listener = myListener;
