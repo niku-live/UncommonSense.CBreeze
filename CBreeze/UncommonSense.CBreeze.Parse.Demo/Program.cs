d794d957926995fe77a4e5d98b61600a5dca82bb
Karolis Uosis <nku2007@hotmail.com>
2019-03-09 19:14:39 +0200

    Demo update

--- CBreeze/UncommonSense.CBreeze.Parse.Demo/Program.cs
+++ CBreeze/UncommonSense.CBreeze.Parse.Demo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using UncommonSense.CBreeze.Read;
+using UncommonSense.CBreeze.Write;
 
 namespace UncommonSense.CBreeze.Parse.Demo
 {
@@ -14,11 +16,21 @@ namespace UncommonSense.CBreeze.Parse.Demo
             var desktopFolderName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             string folderFromArgs = (args != null) && (args.Length > 0) ? args[0] : null;
             var sourceFolderName = folderFromArgs ?? Path.Combine(desktopFolderName, "be");
+            string outputFile = (args != null) && (args.Length > 1) ? args[1] : null;
 
-            var parser = new Parser();
-            parser.Listener = myListener;
-            parser.FileEncoding = Encoding.GetEncoding(775);
-            parser.ParseFiles(Directory.GetFiles(sourceFolderName, "*.txt", SearchOption.AllDirectories));
+            if (String.IsNullOrEmpty(outputFile))
+            {
+                var parser = new Parser();
+                parser.Listener = myListener;
+                parser.FileEncoding = Encoding.GetEncoding(775);
+                parser.ParseFiles(Directory.GetFiles(sourceFolderName, "*.txt", SearchOption.AllDirectories));
+            }
+            else
+            {
+                var encoding = Encoding.GetEncoding(775);
+                var application = ApplicationBuilder.ReadFromFolder(sourceFolderName, encoding);
+                ApplicationWriter.WriteToFile(application, outputFile, encoding);
+            }
         }
     }
 }
